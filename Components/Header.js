9e6234fd99eee3010ccdedf370ca3df30b6614c0
Dylan <hello@erendirasboutique.com>
2026-06-30 16:36:05 -0700

    Add Header component for site header

--- Components/Header.js
+++ Components/Header.js
@@ -1,1 +1,13 @@
-
+export default function Header() {
+  return (
+    <header className="siteHeader">
+      <div className="brand">
+        <img src="/logo.jpeg" alt="Erendira's Boutique" />
+        <div>
+          <h1>Returns Portal</h1>
+          <p>Erendira&apos;s Boutique</p>
+        </div>
+      </div>
+    </header>
+  );
+}
